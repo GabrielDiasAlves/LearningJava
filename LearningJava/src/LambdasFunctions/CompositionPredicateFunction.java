b420ff57e3f57b9173a59e7343f1709d57dcfebd
Gabriel Dias Alves <gabrieldalves@outlook.com.br>
2021-06-01 21:24:49 -0300

    Add comments

--- LearningJava/src/LambdasFunctions/CompositionPredicateFunction.java
+++ LearningJava/src/LambdasFunctions/CompositionPredicateFunction.java
@@ -6,15 +6,14 @@ public class CompositionPredicateFunction {
 	
 	public static void main(String[] args) {
 		
-		Predicate<Integer> isPar = num -> num % 2 == 0;
+		Predicate<Integer> isEven = num -> num % 2 == 0;
 		Predicate<Integer> isThreeDigitos = num -> num >= 100 && num <= 999;
 		
-		System.out.println(isPar.and(isThreeDigitos).test(122));
+		System.out.println(isEven.and(isThreeDigitos).test(122));
 		
-		System.out.println(isPar.or(isThreeDigitos).test(331));
+		System.out.println(isEven.or(isThreeDigitos).test(331));
 		
-		//In this case, it was possible use two checks, "isPar" and "isThreeDigitos", because it's "Boolean"
-		
+		//In this case, it was possible use two checks, "isEven" and "isThreeDigitos", because it's "Boolean"
 		
 	}
 
---
+++ LearningJava/src/LambdasFunctions/Supplier.java
@@ -0,0 +1,14 @@
+package LambdasFunctions;
+/*
+ * Doesn't receive any parameters, but return values
+ * 
+ */
+
+public class Supplier {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
