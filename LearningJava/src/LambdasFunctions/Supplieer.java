de959468cc39cc146d438b46c01f0dd39d208854
Gabriel Dias Alves <gabrieldalves@outlook.com.br>
2021-07-21 21:28:30 -0300

    Added comments

--- LearningJava/src/LambdasFunctions/Supplieer.java
+++ LearningJava/src/LambdasFunctions/Supplieer.java
@@ -1,14 +1,24 @@
 package LambdasFunctions;
+
 /*
  * Doesn't receive any parameters, but return values
- * 
+ * The more specific in this case, better understanding of the usefulness 
  */
 
-public class Supplier {
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
+
+public class Supplieer {
 	
 	public static void main(String[] args) {
 		
+		Supplier<List<String>> justList = () -> Arrays.asList("Gabriel", "Samnuel", "Daniel");
+		//or
+		Supplier<Object> justListOb = () -> Arrays.asList("Gabriel", "Samnuel", "Daniel");
 		
+		System.out.println("String: "+justList.get());
+		System.out.println(" or ");
+		System.out.println("Objects: "+ justListOb.get());
 	}
-
 }
