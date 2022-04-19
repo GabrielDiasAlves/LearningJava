53a3fe046d347e9c72d34f65dd0a68c2f8792793
Gabriel <gabrieldalves@outlook.com.br>
2022-04-18 22:14:12 -0300

    Created a external arquive for connection

--- LearningJava/src/jdbc/ConnectionFactory.java
+++ LearningJava/src/jdbc/ConnectionFactory.java
@@ -1,20 +1,35 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
 	public static Connection getConnection() {
 		try {
-			final String url = "jdbc:mysql://localhost:3307/course_of_java?allowPublicKeyRetrieval=true&useSSL=false";
-			final String user = "root";
-			final String pass = "TestDB@22#";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("database.url");
+			final String user = prop.getProperty("database.user");
+			final String pass = prop.getProperty("database.pass");
+
+//			final String url = "jdbc:mysql://localhost:3307/course_of_java?allowPublicKeyRetrieval=true&useSSL=false";
+//			final String user = "root";
+//			final String pass = "TestDB@22#";
 
 			return DriverManager.getConnection(url, user, pass);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String path = "/connection.properties";
+
+		prop.load(ConnectionFactory.class.getResourceAsStream(path));
+		return prop;
 	}
 }
